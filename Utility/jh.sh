79e47624d3fe26d93f0d97c4db91efa2e2fe6590
kasalic <rdb@dwr.co.uk>
2018-08-03 16:01:47 +0100

    turned into template

--- Utility/jh.sh
+++ Utility/jh.sh
@@ -3,14 +3,13 @@
 jh='/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper'
 jh_args=(\
     -windowType utility \
-    -title "FileVault 2 Encryption" \
-    -heading "Important" \
+    -title "Enter title here" \
+    -heading "Enter heading here" \
     -button1 "OK" \
     -icon /System/Library/CoreServices/Installer.app/Contents/Resources/Installer.icns \
     -alignHeading left \
     -description\
     )
-message="In order to complete FileVault setup, please enter your password when prompted. \
-	It is important that you complete this step to enable us to ensure the data on you Mac is protected."
+message="Enter message for screen here."
 
 "$jh" "${jh_args[@]}" "$message"
