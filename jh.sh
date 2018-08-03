80547e18fe74a92d116a4fa4486d4c56741b8ab7
kasalic <rdb@dwr.co.uk>
2018-08-03 15:39:26 +0100

    Updated to use jamfHelper instead of osascript

--- jh.sh
+++ jh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-FV2USERS="$(fdesetup list)"
 jh='/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper'
 jh_args=(\
     -windowType utility \
@@ -15,13 +13,4 @@ jh_args=(\
 message="In order to complete FileVault setup, please enter your password when prompted. \
 	It is important that you complete this step to enable us to ensure the data on you Mac is protected."
 
-# echo $FV2USERS
-
-if [[ ${FV2USERS} = *"cadmin"* ]] ; 
-then
-    /bin/echo "cadmin is already an FV2 enabled User"
-else
-   "$jh" "${jh_args[@]}" "$message" ; sysadminctl interactive -secureTokenOn cadmin -password itechPW001
-fi
-
-echo $FV2USERS
+"$jh" "${jh_args[@]}" "$message"
