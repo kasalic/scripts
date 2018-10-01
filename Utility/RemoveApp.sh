22ac604d38fb10fd6a6df3c7850ee2ff657531ea
kasalic <rdb@dwr.co.uk>
2018-10-01 11:59:26 +0100

    Uploaded
    Uploaded

---
+++ Utility/RemoveApp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Remove Application Script for Jamf Pro
+# by Graham Pugh
+#
+# This script can delete apps that are sandboxed and live in /Applications
+
+# The first parameter is used to kill the app. It should be the app name or path
+# as required by the pkill command.
+applicationPath="$4"
+
+if [[ -z "${applicationPath}" ]]; then
+    echo "No application specified!"
+    exit 1
+fi
+
+echo "Closing application: ${applicationPath}"
+
+pkill -f "${applicationPath}"
+
+# echo "Removing application: ${applicationPath}"
+
+# rm -rf "/Applications/${applicationPath}.app"
